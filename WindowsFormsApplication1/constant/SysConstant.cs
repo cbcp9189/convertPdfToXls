ce25b62ae46c23d5e969260a962dce998f4183c5
xiaodao <602852363@qq.com>
2017-08-18 11:33:19 +0800

    add constant

---
+++ WindowsFormsApplication1/constant/SysConstant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.constant
+{
+    public class SysConstant
+    {
+        public static byte REPORTS_TYPE = 2;
+
+        public static byte ANNOUNCEMENT_TYPE = 13;
+
+        public static byte NEW_OTC_ANNOUNCEMENT_TYPE = 17;
+
+        public static byte HK_ANNOUNCEMENT_TYPE = 19;
+
+        public static byte WECHAT_ARTICLE_TYPE = 127;
+
+        public static String ANNOUNCEMENT = "GSGGFWB";
+
+        public static String subscriptions = "subscriptions";
+
+
+
+
+
+    }
+}
